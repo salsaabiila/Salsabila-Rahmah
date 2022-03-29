5d146f3833eb9a3e6b67ea8f1677d32acfbfca9d
salsaabiila <89976076+salsaabiila@users.noreply.github.com>
2022-03-29 23:50:57 +0700

    Add files via upload

---
+++ Kelompok-6_SDA-master(3)/Kelompok-6_SDA-master/kelompok 6 sda/src/binaryTree.java
@@ -0,0 +1,22 @@
+
+
+public class binaryTree {
+
+    public String root;
+
+    public void addNode(Node node) {
+    }
+
+    public static String searchValue(binaryTree pencarian, String nama1) {
+        return null;
+    }
+
+    public static String searchValue(String root2, String nama1) {
+        return null;
+    }
+
+    public static String searchValue(String root2, int nama1) {
+        return null;
+    }
+
+}
---
+++ Kelompok-6_SDA-master(3)/Kelompok-6_SDA-master/kelompok 6 sda/src/kel6.java
@@ -0,0 +1,178 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+
+/* Tugas besar kelompok 6
+1). Irfan Wahendra
+2). 
+*/
+
+public class kel6 {
+    static class Node{
+        String key;
+        Node left, right;
+
+        public Node (String string){
+            key = string;
+            left = right = null;
+        } 
+    }
+
+        Node root;
+        kel6(){
+            root = null;
+        }
+
+        void printPreorder(Node node){
+            if(node == null)
+            return;
+
+            System.out.println(node.key + " ");
+            printPreorder(node.left);
+            printPreorder(node.right);
+        }
+
+         void printPreorder(){
+            printPreorder(root);
+        }
+    public static void main(String[] args) {
+        //deklarasi stack beras
+        Stack jumlah_karung = new Stack<String>();//var tambah stok
+        
+
+        //deklarasi qeueu antrian
+        Queue <String> pelanggan = new LinkedList<>();
+        ArrayList<Integer> jumlah_karung1 = new ArrayList<Integer>();
+        
+
+        // deklarasi riwayat
+        ArrayList<String> riwayat_pelanggan1 = new ArrayList <String>();
+        ArrayList <Integer> riwayat_beli =new ArrayList<Integer>();
+        
+
+        kel6 menu = new kel6();
+
+        // bikin binary tree baru
+        binaryTree pencarian = new binaryTree();
+
+        //deklarasi variabel lainnya
+        String pilihan,nama;
+        int jumlah;
+        String nama1;
+
+        //array beras
+        
+
+
+
+
+        System.out.println("===================================");
+        System.out.println("            Program Toko Beras            ");
+        System.out.println("            Halo selamat datang!");
+        System.out.println("===================================\n");
+
+        Scanner inputan = new Scanner(System.in);
+        boolean isLanjutkan = true;
+
+        
+
+        while (isLanjutkan) {
+            System.out.println("Menu pilihan :");
+            menu.root = new Node("1. Cek Stok Beras");
+            menu.root.left = new Node ("2. Tambah Stok Beras");
+            menu.root.left.left = new Node ("3. Tambah Pelanggan");
+            menu.root.left.left.left = new Node ("4. Tampilkan Antrian Pelanggan");
+            menu.root.right = new Node("5. Proses Pembelian");
+            menu.root.right.right = new Node("6. Riwayat Pembelian");
+            menu.root.right.right.right = new Node("7. Tutup");
+        
+            menu.printPreorder();
+
+            System.out.printf("\n\nMasukkan Pilihan  : ");
+            pilihan = inputan.next();
+            System.out.println();
+
+            switch (pilihan) {
+                case "1" :
+                            System.out.println("Cek Stok Beras");
+                            System.out.println("Stok beras Solok: " + jumlah_karung.size());
+                            // System.out.println("Stok beras Solok: " + cisokan);
+                            // System.out.println("\n\n");
+
+                    break;
+                
+                case "2" :
+                            // stack
+                            System.out.print("Masukkan Jumlah Stok : ");
+                            int stokBeras = inputan.nextInt();
+                            for (int x = 0; x < stokBeras; x++){
+                                jumlah_karung.push("1");   
+                            }
+                            // System.out.println("Beras solok : ");
+                            // System.out.println(jumlah_karung);
+                            // for (int j = 1; j <= 1;j++) {
+                            // cisokan.push(j);}
+                            // System.out.println("Beras cisokan : ");
+                            // System.out.println(cisokan);
+
+                            System.out.println("Banyak karung Beras Solok : " + jumlah_karung.size());
+                            // System.out.println("Banyak karung Beras Solok : " + cisokan.size());
+
+                            break;
+                            
+                
+                case "3" :
+                            //Tambah antrian pelanggan
+                            System.out.print("Masukkan nama : ");
+                            nama = inputan.next();
+                            pelanggan.add (nama);
+                            
+                            riwayat_pelanggan1.add(nama);
+                            //System.out.println("Nama pelanggan_jumlah karung : " + nama);
+                            System.out.print("jumlah karung : ");
+                            jumlah = inputan.nextInt();
+                            jumlah_karung1.add (jumlah);
+                            riwayat_beli.add(jumlah);
+                            for (int y=0;y < jumlah;y++){
+                                jumlah_karung.pop();
+                                }
+                                //Pencarian pelanggan
+                                
+                            break;
+                
+                case "4" :
+                            //Tampilkan antrean pelanggan
+                            System.out.println("Antrian pelanggan : " + pelanggan);
+                            break;
+                case "5" :
+                            //Proses pembelian (Queue)
+                            System.out.println("Pembelian atas nama : " + pelanggan.poll());
+                            
+                            break;
+
+                case "6" :
+                            //Riwayat pelanggan
+                            System.out.println(riwayat_pelanggan1);
+                            System.out.println(riwayat_beli);
+                            break;
+                case "7" :
+                    System.exit(0);
+                
+                default:
+                    break;
+                
+            }
+            System.out.print("\nApakah Anda Masih Ingin Lanjut[y/n]? ");
+            pilihan = inputan.next();
+            isLanjutkan = pilihan.equalsIgnoreCase ("y");
+        }
+
+
+
+    }
+    
+}
